3315b5a424e300fe39c806236f0a26913ed09bd7
TheRandomLegoBrick <legomatthew.516@gmail.com>
2022-07-19 12:15:52 -0700

    Navbar experiments

---
+++ scripts/main.js
@@ -0,0 +1,57 @@
+var scrabledElements = document.getElementsByClassName("scrabled")
+var sidebar = document.getElementById("sidebar")
+function lerp(value1, value2, amt) {
+	return ((value2 - value1) * amt) + value1;
+}
+const chars = "ΑαΒβΓγΔδΕεΖζΗηΘθΙιΚκΛλΜμΝνΞξΟοΠπΡρΣσ/ςΤτΥυΦφΧχΨψΩω"
+var sidebarHover = false
+
+function scrabledText(length){
+	let floor = Math.floor
+	let random = Math.random
+	let string = ""
+
+	for(let i = 0; i < length; i++){
+		string += chars[floor(random()*chars.length)]
+	}
+
+	return string
+}
+
+var count = 0;
+var sbpos = 0;
+var sin = Math.sin;
+var bounce = 0;
+var mainloop = window.setInterval(() => {
+
+	if(count%10 === 0){
+		for(let i = 0; i < scrabledElements.length; i++){
+			scrabledElements[i].innerText = scrabledText(7)
+		}
+	}
+
+	if(sidebarHover){
+		sbpos = lerp(sbpos, -50 + sin(count*0.3)*bounce, 0.2);
+	}else{
+		sbpos = lerp(sbpos, -275 + sin(count*0.3)*bounce, 0.2);
+	}
+
+	bounce = Math.max(0, lerp(bounce, -1, 0.1));
+	sidebar.style.left = sbpos+"px";
+
+	count ++;
+}, 1000/60)
+
+sidebar.addEventListener('mouseover', (event) => {
+	if(bounce <= 0){
+		bounce = 200;
+		sidebarHover = true;
+		count = 0;
+	}
+})
+
+sidebar.addEventListener('mouseleave', (event) => {
+	sidebarHover = false;
+	bounce = 200;
+	count = 5;
+})
